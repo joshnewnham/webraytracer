cdfee1b7411ad9b77c81d1b233f586876dc4780f
Joshua Newnham <joshn@mastersofpie.com>
2017-03-02 18:45:49 +0000

    WIP

--- js/entities.js
+++ js/entities.js
@@ -3,21 +3,81 @@
  */
 
 class Entity{
-    constructor(){
+    constructor(position = new Vector()){
         this.name = "Entity";
         this.type = "Entity";
 
-        this.position = new Vector(0, 0, 0);
+        this.position = position;
     }
+}
+
+class SceneEntity extends Entity{
+    constructor(surface=null){
+        super();
+    }
+
+    /**
+     *
+     * @param ray
+     * @returns ISect
+     */
+    intersect(ray){
+        throw "not implemented";
+    }
+
+    /**
+     *
+     * @param position
+     * @returns Vector
+     */
+    normal(position){
+        throw "not implemented";
+    }
+
 }
 
 class Camera extends Entity{
-    constructor(){
-        super();
+    constructor(position = new Vector(), forward = new Vector3(0, 0, 1), up = new Vector(0, 1, 0), right = new Vector(1, 0, 0)){
+        super(position);
         this.type = "Camera";
 
-        this.forward = new Vector3(0, 0, 1);
-        this.up = new Vector(0, 1, 0);
-        this.right = new Vector(1, 0, 0);
+        this.forward = forward;
+        this.up = up;
+        this.right = right;
+    }
+
+    static Create(position, lookAt){
+        var forward = Vector.Norm(Vector.Subtract(lookAt, position));
+        var down = new Vector(0, -1.0, 0);
+        var right = Vector.Scale(Vector.Norm(Vector.Cross(forward, down)), 1.5);
+        var up = Vector.Scale(Vector.Norm(Vector.Cross(forward, right)), 1.5);
+
+        return new Camera(position, forward, up, right);
     }
 }
+
+class Light extends Entity{
+    constructor(position = new Vector(), color = new Color()){
+        super(position);
+        this.type = "Light";
+        this.color = color;
+    }
+}
+
+class ISect {
+    constructor(sceneObject=null, ray=null, distance=0.0){
+        this.sceneObject = sceneObject;
+        this.ray = ray;
+        this.distance = distance;
+    }
+}
+
+class Surface{
+    constructor(diffuseFunc=null, specularFunc=null, reflectFunc=null, roughness=0.0){
+        this.diffuseFunc = diffuseFunc;
+        this.specularFunc = specularFunc;
+        this.reflectFunc = reflectFunc;
+        this.roughness = roughness;
+    }
+}
+
--- js/mathutils.js
+++ js/mathutils.js
@@ -3,7 +3,7 @@
  */
 
 class Vector{
-    constructor(x, y, z){
+    constructor(x=0.0, y=0.0, z=0.0){
         this.x = x;
         this.y = y;
         this.z = z;
@@ -13,11 +13,11 @@ class Vector{
         return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
     }
 
-    static MakeXYZ(x, y, z) {
+    static CreateXYZ(x, y, z) {
         return new Vector(x, y, z);
     }
 
-    static MakeFromString(xyz) {
+    static CreateFromString(xyz) {
         var parts = xyz.split(",");
         x = 0.0;
         y = 0.0;
@@ -68,7 +68,7 @@ class Vector{
         return new Vector(v1.x * scale, v1.y * scale, v1.z * scale);
     }
 
-    static norm(v)
+    static Norm(v)
     {
         var len = v.length();
         var div = len == 0 ? Number.MAX_VALUE : 1 / div;
@@ -77,5 +77,69 @@ class Vector{
 
     static Equals(v1, v2){
         return v1.x == v2.x && v1.y == v2.y && v1.z == v2.z;
+    }
+}
+
+class Color{
+    constructor(r=1.0, g=1.0, b=1.0, a=1.0){
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.a = a;
+    }
+
+    legalize(d) {
+        return d > 1 ? 1 : d;
+    }
+
+    static Make(r=1.0, g=1.0, b=1.0, a=1.0){
+        return new Color(r, g, b, a);
+    }
+
+    static White(){
+        return new Color(1, 1, 1, 1);
+    }
+
+    static Black(){
+        return new Color(0, 0, 0, 1);
+    }
+
+    static Red(){
+        return new Color(1, 0, 0, 1);
+    }
+
+    static Green(){
+        return new Color(0, 1, 0, 1);
+    }
+
+    static Blue(){
+        return new Color(0, 0, 1, 1);
+    }
+
+    static Add(c1, c2){
+        return new Color(c1.r + c2.r, c1.g + c2.g, c1.b + c2.b, c1.a + c2.a);
+    }
+
+    static Subtract(c1, c2){
+        return new Color(c1.r - c2.r, c1.g - c2.g, c1.b - c2.b, c1.a - c2.a);
+    }
+
+    static Multiply(c1, v2){
+        return new Color(c1.r * c2.r, c1.g * c2.g, c1.b * c2.b, c1.a * c2.a);
+    }
+
+    static Divide(c1, c2){
+        return new Color(c1.r / c2.r, c1.g / c2.g, c1.b / c2.b, c1.a / c2.a);
+    }
+
+    static Scale(c, scale){
+        return new Color(c.x * scale, c.y * scale, c.z * scale);
+    }
+}
+
+class Ray{
+    constructor(position = new Vector(), direction = new Vector()){
+        this.position = position;
+        this.dir = direction;
     }
 }
